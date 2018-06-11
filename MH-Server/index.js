9c856b15aab4be48aab35fe5f85cb345c8248220
Adam Summerfield <summerfield.adam@gmail.com>
2018-06-11 17:05:55 +0200

    Added dependencies

---
+++ MH-Server/index.js
@@ -0,0 +1,4 @@
+// Load dependencies 
+const app = express();                      // Express framework 
+const bodyParser = require('body-parser');  // JSON Parser
+const helmet = require('helmet');           // Protection against well known vulnerabilities
