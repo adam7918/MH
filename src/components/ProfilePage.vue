6e9aad6d8f6632b4d68bf55f6c8c231c67cc6ce1
Adam Summerfield <summerfield.adam@gmail.com>
2018-07-18 00:46:25 +0200

    Auto deployment test..

--- src/components/ProfilePage.vue
+++ src/components/ProfilePage.vue
@@ -4,3 +4,37 @@
 
     </main>
 </template>
+<script>
+import axios from 'axios'
+
+export default {
+    data() {
+      return {
+        // User login details
+        loginUsername: '',
+        loginPassword: '',
+
+        // User registration details
+        registerUsername: '',
+        registerEmail: '',
+        registerPassword: '',
+
+        // Contains error message regarded login/registration status
+        formErrorMessage: '',
+
+        registrationSuccessMessage: '',
+
+        // False = Login, True = Registering
+        registerMode: false,
+      }
+    },
+    created() {
+        alert("Testing")
+
+    },
+    
+    
+  
+  
+}
+</script>
