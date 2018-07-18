98359c68eebb0698d0b036a4235eeb8896fcf9ad
Adam Summerfield <summerfield.adam@gmail.com>
2018-07-18 10:07:54 +0200

    Testing ad

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,12 +2,12 @@
     <main>
         <header class="nav">
             <div class="nav__left">
-                <h1 class="nav__title">MMedieval<span>Havoc</span> </h1>
+                <h1 class="nav__title">Medieval<span>Havoc</span> </h1>
             </div>
             <div class="nav__right">
                 <a>Rankings</a>
                 <a>0 Online</a>
-                <button class="nav__button">Play Now</button>
+                <button class="nav__button">Play</button>
             </div>
         </header>
     </main>
