4a56d77a41e6ddf6f1661d8c03a95f076e91315f
Adam Summerfield <summerfield.adam@gmail.com>
2018-07-21 13:05:07 +0200

    updated route url

--- MH-Server/routes/accountRoutes.js
+++ MH-Server/routes/accountRoutes.js
@@ -8,7 +8,7 @@ router.get('/:id', account_controller.view_account);
 router.post('/', account_controller.add_account);
 router.put('/edit/:username/', account_controller.edit_account);
 router.post('/login/', account_controller.login)
-router.get('/online/', account_controller.get_count_users_online);
+router.get('/online/all', account_controller.get_count_users_online);
 router.put('/online/', account_controller.update_last_online);
 
 module.exports = router;
