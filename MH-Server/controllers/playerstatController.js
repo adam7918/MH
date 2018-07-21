f7b9747a42861aa088c855ba48fdc859b233696d
Adam Summerfield <summerfield.adam@gmail.com>
2018-07-21 19:33:43 +0200

    testing change

--- MH-Server/controllers/playerstatController.js
+++ MH-Server/controllers/playerstatController.js
@@ -6,7 +6,7 @@ const config = require('../config');
 const token = require('../routes/jwtHandler');
 
 exports.view_playerstats_by_id = function (req, res) {
-    pool.query('SELECT * FROM player_stats WHERE username = as', function (err, rows) {
+    pool.query('SELECT * FROM player_stats WHERE username = `as`', function (err, rows) {
         if (err) {
             res.status(400).json(err);
         }
