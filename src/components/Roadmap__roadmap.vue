215f91ce52dfc22f3717f62433052703f2d9303e
Adam Summerfield <summerfield.adam@gmail.com>
2018-07-22 15:32:37 +0200

    Added roadmap

--- src/components/Roadmap__roadmap.vue
+++ src/components/Roadmap__roadmap.vue
@@ -1,5 +1,40 @@
 <template>
-    <div class="placeholderdiv">
-        Roadmap - Coming soon.
+    <div class="content">
+        <div class="roadmap-introduction">
+            <p>This is our roadmap for upcoming features. Visit our discord for more discussion: <a href="https://discord.gg/nxg5bGw" target="_blank">Here</a></p>
+        </div>
+        <div class="roadmap">
+            <div class="roadmap__column">
+                <div class="roadmap__title">
+                    <p class="roadmap__title--red">Upcoming</p>
+                </div>
+                <div class="roadmap__feature">
+                    <h2>Guilds</h2>
+                    <p>Players can form guilds.</p>
+                </div>
+                <div class="roadmap__feature">
+                    <h2>Mailing</h2>
+                    <p>Send and receive mail.</p>
+                </div>
+            </div>
+            <div class="roadmap__column">
+                <div class="roadmap__title">
+                    <p class="roadmap__title--orange">In development</p>
+                </div>
+                <div class="roadmap__feature">
+                    <h2>Rankings</h2>
+                    <p>Track the best players of Medieval Havoc.</p>
+                </div>
+            </div>
+            <div class="roadmap__column">
+                <div class="roadmap__title">
+                    <p class="roadmap__title--green">Live</p>
+                </div>
+                <div class="roadmap__feature">
+                    <h2>Online Players</h2>
+                    <p>Track how many players are currently online.</p>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
