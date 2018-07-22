3bd3221e3e48b6fc47ba17836e7dcfa88e51432e
Adam Summerfield <summerfield.adam@gmail.com>
2018-07-22 22:55:35 +0200

    Added client-side only online users page

--- src/components/UserStatPage__online.vue
+++ src/components/UserStatPage__online.vue
@@ -1,5 +1,53 @@
 <template>
-    <div class="placeholderdiv">
-        Online users - Coming soon.
+    <div class="content">
+        <div class="content-introduction">
+            <p>There are <span class="color--white">6</span> players online.</p>
+        </div>
+        <div class="online-users">
+           <div class="online-users-panel">
+               <img src="https://img.sndimg.com/food/image/upload/fl_progressive,e_brightness:15,w_200,h_200,c_fill,q_92/v1/fdc/img/placeholder/fdc-generic-avatar.jpg"/>
+               <div class="online-users-panel__text">
+                   <h2><span class="online-users-panel--level">Lvl 23.</span> Azgard</h2>
+                   <p>Last active 23 minutes ago</p>
+               </div>
+           </div><div class="online-users-panel">
+               <img src="https://img.sndimg.com/food/image/upload/fl_progressive,e_brightness:15,w_200,h_200,c_fill,q_92/v1/fdc/img/placeholder/fdc-generic-avatar.jpg"/>
+               <div class="online-users-panel__text">
+                   <h2><span class="online-users-panel--level">Lvl 23.</span> Azgard</h2>
+                   <p>Last active 23 minutes ago</p>
+               </div>
+           </div>
+            <div class="online-users-panel">
+                <img src="https://img.sndimg.com/food/image/upload/fl_progressive,e_brightness:15,w_200,h_200,c_fill,q_92/v1/fdc/img/placeholder/fdc-generic-avatar.jpg"/>
+                <div class="online-users-panel__text">
+                    <h2><span class="online-users-panel--level">Lvl 23.</span> Azgard</h2>
+                    <p>Last active 23 minutes ago</p>
+                </div>
+            </div>
+        
+            <div class="online-users-panel">
+                <img src="https://img.sndimg.com/food/image/upload/fl_progressive,e_brightness:15,w_200,h_200,c_fill,q_92/v1/fdc/img/placeholder/fdc-generic-avatar.jpg"/>
+                <div class="online-users-panel__text">
+                    <h2><span class="online-users-panel--level">Lvl 23.</span> Azgard</h2>
+                    <p>Last active 23 minutes ago</p>
+                </div>
+            </div>
+            <div class="online-users-panel">
+                <img src="https://img.sndimg.com/food/image/upload/fl_progressive,e_brightness:15,w_200,h_200,c_fill,q_92/v1/fdc/img/placeholder/fdc-generic-avatar.jpg"/>
+                <div class="online-users-panel__text">
+                    <h2><span class="online-users-panel--level">Lvl 23.</span> Azgard</h2>
+                    <p>Last active 23 minutes ago</p>
+                </div>
+            </div>
+            <div class="online-users-panel">
+                <img src="https://img.sndimg.com/food/image/upload/fl_progressive,e_brightness:15,w_200,h_200,c_fill,q_92/v1/fdc/img/placeholder/fdc-generic-avatar.jpg"/>
+                <div class="online-users-panel__text">
+                    <h2><span class="online-users-panel--level">Lvl 23.</span> Azgard</h2>
+                    <p>Last active 23 minutes ago</p>
+                </div>
+            </div>
+          
+            
+        </div>
     </div>
 </template>
