9217d2ea2db44de201f80865a000b62c717e7c91
Adam Summerfield <summerfield.adam@gmail.com>
2018-07-23 21:33:50 +0200

    HTTP rate limit set to 3000 request per 15 mins

--- MH-Server/index.js
+++ MH-Server/index.js
@@ -13,7 +13,7 @@ var RateLimit = require('express-rate-limit');
 
 var limiter = new RateLimit({
   windowMs: 15*60*1000, // 15 minutes
-  max: 10, // limit each IP to 100 requests per windowMs
+  max: 3000, // limit each IP to 100 requests per windowMs
   delayMs: 0, // disable delaying - full speed until the max limit is reached
 });
 
