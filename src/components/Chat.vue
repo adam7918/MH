5598247f261a12960f2173751d6eee83d222b373
Adam Summerfield <summerfield.adam@gmail.com>
2018-07-24 00:55:03 +0200

    socket.io test

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,5 +1,45 @@
 <template>
     <div class="chat-tab">
-        World Chat (0)
+        <form @submit.prevent="sendMessage">
+            <div class="gorm-group">
+                <label for="user">User:</label>
+                <input type="text" v-model="user" class="form-control">
+            </div>
+            <div class="gorm-group pb-3">
+                <label for="message">Message:</label>
+                <input type="text" v-model="message" class="form-control">
+            </div>
+            <button type="submit" class="btn btn-success">Send</button>
+        </form>
     </div>
 </template>
+<script>
+import io from 'socket.io-client';
+export default {
+    data() {
+        return {
+            user: '',
+            message: '',
+            messages: [],
+            socket : io('http://www.medievalhavoc.com:3000')
+        }
+    },
+    methods: {
+        sendMessage(e) {
+            e.preventDefault();
+            
+            this.socket.emit('SEND_MESSAGE', {
+                user: this.user,
+                message: this.message
+            });
+            this.message = ''
+        }
+    },
+    mounted() {
+        this.socket.on('MESSAGE', (data) => {
+            this.messages = [...this.messages, data];
+            // you can also do this.messages.push(data)
+        });
+    }
+}
+</script>
