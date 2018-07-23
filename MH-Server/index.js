a51104bea27feef74d157f8f1fa475d86ed53adf
Adam Summerfield <summerfield.adam@gmail.com>
2018-07-23 21:26:49 +0200

    #8

--- MH-Server/index.js
+++ MH-Server/index.js
@@ -16,7 +16,7 @@ var limiter = new RateLimit({
   windowMs: 15*60*1000, // 15 minutes
   max: 10, // limit each IP to 100 requests per windowMs
   delayMs: 0, // disable delaying - full speed until the max limit is reached
-  handler: function(req, res, /*next*/){
+  handler: function(req, res /*next*/){
     if (options.headers) {
         res.setHeader('Retry-After', Math.ceil(options.windowMs / 1000));
       }
