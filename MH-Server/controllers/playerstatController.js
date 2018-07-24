6ab222431c486c833140e2ca053df2b238f60c89
Adam Summerfield <summerfield.adam@gmail.com>
2018-07-25 01:27:34 +0200

    fixed missing level on wealth rankings

--- MH-Server/controllers/playerstatController.js
+++ MH-Server/controllers/playerstatController.js
@@ -29,7 +29,7 @@ exports.view_rankings_for_level = function (req, res) {
 }
 
 exports.view_rankings_for_gold = function (req, res) {
-    pool.query("SELECT a.username, a.last_online, p.`gold` FROM account a INNER JOIN player_stats p ON a.username = p.username ORDER BY p.gold DESC LIMIT 50", function (err, row) {
+    pool.query("SELECT a.username, a.last_online, p.`gold`, p.`level` FROM account a INNER JOIN player_stats p ON a.username = p.username ORDER BY p.gold DESC LIMIT 50", function (err, row) {
         if (err) {
             res.status(400).json({ error: err });
         } else if (row.length == 0) {
@@ -41,7 +41,7 @@ exports.view_rankings_for_gold = function (req, res) {
 }
 
 exports.view_rankings_for_rubies = function (req, res) {
-    pool.query("SELECT a.username, a.last_online, p.`rubies` FROM account a INNER JOIN player_stats p ON a.username = p.username ORDER BY p.rubies DESC LIMIT 50", function (err, row) {
+    pool.query("SELECT a.username, a.last_online, p.`rubies`, p.`level` FROM account a INNER JOIN player_stats p ON a.username = p.username ORDER BY p.rubies DESC LIMIT 50", function (err, row) {
         if (err) {
             res.status(400).json({ error: err });
         } else if (row.length == 0) {
