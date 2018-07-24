68ffed2d15b052d2a747420bb710a7291d9e6628
Adam Summerfield <summerfield.adam@gmail.com>
2018-07-24 17:25:14 +0200

    Updated roadmap

--- src/components/Roadmap__roadmap.vue
+++ src/components/Roadmap__roadmap.vue
@@ -10,20 +10,72 @@
                 </div>
                 <div class="roadmap__feature">
                     <h2>Guilds</h2>
-                    <p>Players can form guilds.</p>
+                    <p>Form guilds with other players.</p>
                 </div>
                 <div class="roadmap__feature">
                     <h2>Mailing</h2>
                     <p>Send and receive mail.</p>
                 </div>
+                <div class="roadmap__feature">
+                    <h2>Inventory</h2>
+                    <p>View the items you own.</p>
+                </div>
+                <div class="roadmap__feature">
+                    <h2>Loadout</h2>
+                    <p>Choose your equipped gear and rune loadout.</p>
+                </div>
+                <div class="roadmap__feature">
+                    <h2>Daily Gift</h2>
+                    <p>Claim a reward for logging in each day.</p>
+                </div>
+                <div class="roadmap__feature">
+                    <h2>Profiles</h2>
+                    <p>Each user will have their own profile page displaying their information and greatest accomplishments.</p>
+                </div>
+                <div class="roadmap__feature">
+                    <h2>Custom Avatars</h2>
+                    <p>Choose a custom avatar for your profile.</p>
+                </div>
+                <div class="roadmap__feature">
+                    <h2>Feature Voting</h2>
+                    <p>Vote on which roadmap feature you want to be implemented next.</p>
+                </div>
+                <div class="roadmap__feature">
+                    <h2>Friends List</h2>
+                    <p>Add friends so you can access them quicker.</p>
+                </div>
+                <div class="roadmap__feature">
+                    <h2>Vendors</h2>
+                    <p>Purchase items from vendors with gold or rubies.</p>
+                </div>
+                <div class="roadmap__feature">
+                    <h2>Tower of Havoc</h2>
+                    <p>Growing with difficulty the higher the floor, the Tower of Havoc is a true test of a player's strength.</p>
+                </div>
+                <div class="roadmap__feature">
+                    <h2>Mobile Responsiveness</h2>
+                    <p>Mobile and tablet support.</p>
+                </div>
+                <div class="roadmap__feature">
+                    <h2>Notification Log</h2>
+                    <p>View all your notifications.</p>
+                </div>
+                <div class="roadmap__feature">
+                    <h2>Market</h2>
+                    <p>Sell items to other players in the market.</p>
+                </div>
+                <div class="roadmap__feature">
+                    <h2>Trading</h2>
+                    <p>Trade items with other players.</p>
+                </div>
+                <div class="roadmap__feature">
+                    <h2>Landing Page</h2>
+                    <p>Landing page for Medieval Havoc.</p>
+                </div>
             </div>
             <div class="roadmap__column">
                 <div class="roadmap__title">
                     <p class="roadmap__title--orange">In development</p>
-                </div>
-                <div class="roadmap__feature">
-                    <h2>Rankings</h2>
-                    <p>Track the best players of Medieval Havoc.</p>
                 </div>
             </div>
             <div class="roadmap__column">
@@ -31,8 +83,16 @@
                     <p class="roadmap__title--green">Live</p>
                 </div>
                 <div class="roadmap__feature">
-                    <h2>Online Players</h2>
-                    <p>Track how many players are currently online.</p>
+                    <h2>Player Lists</h2>
+                    <p>View a list of all players, players that are online and also some game statistics.</p>
+                </div>
+                <div class="roadmap__feature">
+                    <h2>World Chat</h2>
+                    <p>Chat with other players who are online.</p>
+                </div>
+                <div class="roadmap__feature">
+                    <h2>Rankings</h2>
+                    <p>View the top wealthiest and strongest players.</p>
                 </div>
             </div>
         </div>
