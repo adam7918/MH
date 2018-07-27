31041ae1d8b8383212a8a52e5d7c16008b965f84
Adam Summerfield <summerfield.adam@gmail.com>
2018-07-27 13:34:01 +0200

    Added inventory structure

--- src/components/InventoryPage__inventory.vue
+++ src/components/InventoryPage__inventory.vue
@@ -1,39 +1,31 @@
 <template>
     <div class="content">
         <div class="ranking-filters-container">
-            <p>Filters</p>
+            <p>Type</p>
             <div class="ranking-filters-buttons">
-                <p :class="{'ranking-filters-buttons-active': activeFilter === 1 }" @click="activeFilter = 1">All</p>
-                <p :class="{'ranking-filters-buttons-active': activeFilter === 2 }" @click="activeFilter = 2">Weapon</p>
-                <p :class="{'ranking-filters-buttons-active': activeFilter === 3 }" @click="activeFilter = 3">Helmet</p>
-                <p :class="{'ranking-filters-buttons-active': activeFilter === 4 }" @click="activeFilter = 4">Torso</p>
-                <p :class="{'ranking-filters-buttons-active': activeFilter === 5 }" @click="activeFilter = 5">Gauntlets</p>
-                <p :class="{'ranking-filters-buttons-active': activeFilter === 6 }" @click="activeFilter = 6">Greaves</p>
-                <p :class="{'ranking-filters-buttons-active': activeFilter === 7 }" @click="activeFilter = 7">Amulet</p>
-                <p :class="{'ranking-filters-buttons-active': activeFilter === 8 }" @click="activeFilter = 8">Runes</p>
-                <p :class="{'ranking-filters-buttons-active': activeFilter === 9 }" @click="activeFilter = 9">Misc</p>
+                <p :class="{'ranking-filters-buttons-active': activeType === 0 }" @click="activeType = 0">All</p>
+                <p :class="{'ranking-filters-buttons-active': activeType === 1 }" @click="activeType = 1">Weapon</p>
+                <p :class="{'ranking-filters-buttons-active': activeType === 2 }" @click="activeType = 2">Helmet</p>
+                <p :class="{'ranking-filters-buttons-active': activeType === 3 }" @click="activeType = 3">Torso</p>
+                <p :class="{'ranking-filters-buttons-active': activeType === 4 }" @click="activeType = 4">Gauntlets</p>
+                <p :class="{'ranking-filters-buttons-active': activeType === 5 }" @click="activeType = 5">Greaves</p>
+                <p :class="{'ranking-filters-buttons-active': activeType === 6 }" @click="activeType = 6">Amulet</p>
+                <p :class="{'ranking-filters-buttons-active': activeType === 7 }" @click="activeType = 7">Runes</p>
+                <p :class="{'ranking-filters-buttons-active': activeType === 8 }" @click="activeType = 8">Misc</p>
+            </div>
+        </div>
+        <div class="ranking-filters-container" style="margin-top: -30px">
+            <p>Rarity</p>
+            <div class="ranking-filters-buttons">
+                <p :class="{'ranking-filters-buttons-active': activeRarity === 0 }" @click="activeRarity = 0">All</p>
+                <p :class="{'ranking-filters-buttons-active': activeRarity === 1 }" @click="activeRarity = 1">Common</p>
+                <p :class="{'ranking-filters-buttons-active': activeRarity === 2 }" @click="activeRarity = 2">Epic</p>
+                <p :class="{'ranking-filters-buttons-active': activeRarity === 3 }" @click="activeRarity = 3">Legendary</p>
             </div>
         </div>
         <div class="inventory-container">
-            <div class="inventory-item item-orange inventory-item--selected">
-            </div>
-            <div class="inventory-item item-blue">
-            </div>
-            <div class="inventory-item item-purple">
-            </div>
-            <div class="inventory-item item-purple">
-            </div>
-            <div class="inventory-item item-orange">
-            </div>
-            <div class="inventory-item item-blue">
-            </div>
-            <div class="inventory-item item-blue">
-            </div>
-            <div class="inventory-item item-purple">
-            </div>
-            <div class="inventory-item item-orange">
-            </div>
-            <div class="inventory-item item-blue">
+            <div v-if="checkTableFilters(item)" v-for="(item, index) in inventoryItems" :key="index" :class="itemClass[index]" @click="selectedItem = index">
+                {{item.itemName}}
             </div>
         </div>
     </div>
@@ -47,18 +39,348 @@ let $ = jquery;
 export default {
     data() {
       return {
-          activeFilter: 1,
-          inventoryItems: []
+          activeRarity: 0,
+          activeType:0,
+          inventoryItems: [],
+          selectedItem: 99999999
           
       }
+    },
+    computed: {
+        itemClass: function(){
+            let self = this
+            return this.inventoryItems.map(function (item, index) {
+              return {
+                  'inventory-item': true,
+                  'item-blue': (item.itemRarity == 1),
+                  'item-purple': (item.itemRarity == 2),
+                  'item-orange': (item.itemRarity == 3),
+                  'inventory-item--selected': (self.selectedItem == index ? true : false)
+              }
+            });
+            
+        }
     },
     created() {
         if(!this.$auth.checkAuth()){
             this.$router.push('/login')
         }
+
+        this.getInventory()
     },
     methods: {
-
+        getInventory: function(){
+            this.inventoryItems = [
+                {
+                    itemName: 'Sword',
+                    itemLevelReq: 10,
+                    itemType: 1,
+                    itemBonus: 0,
+                    itemRarity: 1,
+                    itemHp: 0,
+                    itemEnergy: 0,
+                    itemStrength: 10,
+                    itemAgility: 10,
+                    itemDefense: 0,
+                    itemEquipped: 0
+                },
+                {
+                    itemName: 'Sword',
+                    itemLevelReq: 10,
+                    itemType: 1,
+                    itemBonus: 0,
+                    itemRarity: 2,
+                    itemHp: 100,
+                    itemEnergy: 0,
+                    itemStrength: 20,
+                    itemAgility: 0,
+                    itemDefense: 0,
+                    itemEquipped: 0
+                },
+                {
+                    itemName: 'Sword',
+                    itemLevelReq: 10,
+                    itemType: 1,
+                    itemBonus: 0,
+                    itemRarity: 3,
+                    itemHp: 0,
+                    itemEnergy: 0,
+                    itemStrength: 30,
+                    itemAgility: 0,
+                    itemDefense: 30,
+                    itemEquipped: 0
+                },
+                {
+                    itemName: 'Helmet',
+                    itemLevelReq: 10,
+                    itemType: 2,
+                    itemBonus: 0,
+                    itemRarity: 1,
+                    itemHp: 0,
+                    itemEnergy: 0,
+                    itemStrength: 10,
+                    itemAgility: 10,
+                    itemDefense: 0,
+                    itemEquipped: 0
+                },
+                {
+                    itemName: 'Helmet',
+                    itemLevelReq: 10,
+                    itemType: 2,
+                    itemBonus: 0,
+                    itemRarity: 2,
+                    itemHp: 100,
+                    itemEnergy: 0,
+                    itemStrength: 20,
+                    itemAgility: 0,
+                    itemDefense: 0,
+                    itemEquipped: 0
+                },
+                {
+                    itemName: 'Helmet',
+                    itemLevelReq: 10,
+                    itemType: 2,
+                    itemBonus: 0,
+                    itemRarity: 3,
+                    itemHp: 0,
+                    itemEnergy: 0,
+                    itemStrength: 30,
+                    itemAgility: 0,
+                    itemDefense: 30,
+                    itemEquipped: 0
+                },
+                {
+                    itemName: 'Torso',
+                    itemLevelReq: 10,
+                    itemType: 3,
+                    itemBonus: 0,
+                    itemRarity: 1,
+                    itemHp: 0,
+                    itemEnergy: 0,
+                    itemStrength: 10,
+                    itemAgility: 10,
+                    itemDefense: 0,
+                    itemEquipped: 0
+                },
+                {
+                    itemName: 'Torso',
+                    itemLevelReq: 10,
+                    itemType: 3,
+                    itemBonus: 0,
+                    itemRarity: 2,
+                    itemHp: 100,
+                    itemEnergy: 0,
+                    itemStrength: 20,
+                    itemAgility: 0,
+                    itemDefense: 0,
+                    itemEquipped: 0
+                },
+                {
+                    itemName: 'Torso',
+                    itemLevelReq: 10,
+                    itemType: 3,
+                    itemBonus: 0,
+                    itemRarity: 3,
+                    itemHp: 0,
+                    itemEnergy: 0,
+                    itemStrength: 30,
+                    itemAgility: 0,
+                    itemDefense: 30,
+                    itemEquipped: 0
+                },
+                {
+                    itemName: 'Gauntlets',
+                    itemLevelReq: 10,
+                    itemType: 4,
+                    itemBonus: 0,
+                    itemRarity: 1,
+                    itemHp: 0,
+                    itemEnergy: 0,
+                    itemStrength: 10,
+                    itemAgility: 10,
+                    itemDefense: 0,
+                    itemEquipped: 0
+                },
+                {
+                    itemName: 'Gauntlets',
+                    itemLevelReq: 10,
+                    itemType: 4,
+                    itemBonus: 0,
+                    itemRarity: 2,
+                    itemHp: 100,
+                    itemEnergy: 0,
+                    itemStrength: 20,
+                    itemAgility: 0,
+                    itemDefense: 0,
+                    itemEquipped: 0
+                },
+                {
+                    itemName: 'Gauntlets',
+                    itemLevelReq: 10,
+                    itemType: 4,
+                    itemBonus: 0,
+                    itemRarity: 3,
+                    itemHp: 0,
+                    itemEnergy: 0,
+                    itemStrength: 30,
+                    itemAgility: 0,
+                    itemDefense: 30,
+                    itemEquipped: 0
+                },
+                {
+                    itemName: 'Greaves',
+                    itemLevelReq: 10,
+                    itemType: 5,
+                    itemBonus: 0,
+                    itemRarity: 1,
+                    itemHp: 0,
+                    itemEnergy: 0,
+                    itemStrength: 10,
+                    itemAgility: 10,
+                    itemDefense: 0,
+                    itemEquipped: 0
+                },
+                {
+                    itemName: 'Greaves',
+                    itemLevelReq: 10,
+                    itemType: 5,
+                    itemBonus: 0,
+                    itemRarity: 2,
+                    itemHp: 100,
+                    itemEnergy: 0,
+                    itemStrength: 20,
+                    itemAgility: 0,
+                    itemDefense: 0,
+                    itemEquipped: 0
+                },
+                {
+                    itemName: 'Greaves',
+                    itemLevelReq: 10,
+                    itemType: 5,
+                    itemBonus: 0,
+                    itemRarity: 3,
+                    itemHp: 0,
+                    itemEnergy: 0,
+                    itemStrength: 30,
+                    itemAgility: 0,
+                    itemDefense: 30,
+                    itemEquipped: 0
+                },
+                {
+                    itemName: 'Amulet',
+                    itemLevelReq: 10,
+                    itemType: 6,
+                    itemBonus: 0,
+                    itemRarity: 1,
+                    itemHp: 0,
+                    itemEnergy: 0,
+                    itemStrength: 10,
+                    itemAgility: 10,
+                    itemDefense: 0,
+                    itemEquipped: 0
+                },
+                {
+                    itemName: 'Amulet',
+                    itemLevelReq: 10,
+                    itemType: 6,
+                    itemBonus: 0,
+                    itemRarity: 2,
+                    itemHp: 100,
+                    itemEnergy: 0,
+                    itemStrength: 20,
+                    itemAgility: 0,
+                    itemDefense: 0,
+                    itemEquipped: 0
+                },
+                {
+                    itemName: 'Amulet',
+                    itemLevelReq: 10,
+                    itemType: 6,
+                    itemBonus: 0,
+                    itemRarity: 3,
+                    itemHp: 0,
+                    itemEnergy: 0,
+                    itemStrength: 30,
+                    itemAgility: 0,
+                    itemDefense: 30,
+                    itemEquipped: 0
+                },
+                {
+                    itemName: 'Runes',
+                    itemLevelReq: 10,
+                    itemType: 7,
+                    itemBonus: 0,
+                    itemRarity: 1,
+                    itemHp: 0,
+                    itemEnergy: 0,
+                    itemStrength: 10,
+                    itemAgility: 10,
+                    itemDefense: 0,
+                    itemEquipped: 0
+                },
+                {
+                    itemName: 'Runes',
+                    itemLevelReq: 10,
+                    itemType: 7,
+                    itemBonus: 0,
+                    itemRarity: 2,
+                    itemHp: 100,
+                    itemEnergy: 0,
+                    itemStrength: 20,
+                    itemAgility: 0,
+                    itemDefense: 0,
+                    itemEquipped: 0
+                },
+                {
+                    itemName: 'Runes',
+                    itemLevelReq: 10,
+                    itemType: 7,
+                    itemBonus: 0,
+                    itemRarity: 3,
+                    itemHp: 0,
+                    itemEnergy: 0,
+                    itemStrength: 30,
+                    itemAgility: 0,
+                    itemDefense: 30,
+                    itemEquipped: 0
+                },
+                {
+                    itemName: 'Energy Potion',
+                    itemLevelReq: 10,
+                    itemType: 8,
+                    itemBonus: 0,
+                    itemRarity: 1,
+                    itemHp: 0,
+                    itemEnergy: 0,
+                    itemStrength: 0,
+                    itemAgility: 0,
+                    itemDefense: 0,
+                    itemEquipped: 0
+                },
+                {
+                    itemName: 'Energy Potion',
+                    itemLevelReq: 10,
+                    itemType: 9,
+                    itemBonus: 0,
+                    itemRarity: 2,
+                    itemHp: 0,
+                    itemEnergy: 0,
+                    itemStrength: 0,
+                    itemAgility: 0,
+                    itemDefense: 0,
+                    itemEquipped: 0
+                },
+            ]
+        },
+        checkTableFilters: function(item){
+            if(this.activeRarity != 0 && item.itemRarity != this.activeRarity){
+                return false
+            } else if (this.activeType != 0 && item.itemType != this.activeType){
+                return false
+            } else {
+                return true
+            }
+        }
         
     }
     
