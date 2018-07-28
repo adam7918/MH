4c43f14fab5f30dd000ad39300da46ff0998a419
Adam Summerfield <summerfield.adam@gmail.com>
2018-07-28 21:02:54 +0200

    Added voting

--- src/components/Roadmap__vote.vue
+++ src/components/Roadmap__vote.vue
@@ -1,5 +1,33 @@
 <template>
     <div class="placeholderdiv">
-        Vote - coming soon.
+        <vue-poll v-bind="options" @addvote="addVote"/>
     </div>
 </template>
+<script> 
+    
+    import VuePoll from 'vue-poll'
+    
+    export default {        
+        data() {
+            return {
+                options: {
+                    question: 'Which <strong>feature</strong> do you want us to implement next?',
+                    answers: [
+                        { value: 1, text: 'Professions', votes: 3 },
+                        { value: 2, text: 'PvP', votes: 2 },
+                        { value: 3, text: 'Land', votes: 1 },
+                        { value: 4, text: 'Crafting', votes: 4 } 
+                    ],
+                    showResults:true
+                }
+            }
+        },
+        components: {
+            VuePoll
+        },
+        methods: {
+            addVote(obj){
+            }
+        }
+    }
+</script>
