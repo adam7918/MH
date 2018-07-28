643bde9a75add8ba836df11d2ef2e6ebd0d9057f
Adam Summerfield <summerfield.adam@gmail.com>
2018-07-28 19:26:25 +0200

    Added another chart

--- src/components/UserStatPage__statistics.vue
+++ src/components/UserStatPage__statistics.vue
@@ -1,17 +1,41 @@
 <template>
     <div class="content">
-        <div class="content-introduction">
-            <p>Playerbase Statistics</p>
+        <div class="ranking-filters-container">
+            <p>Filters</p>
+            <div class="ranking-filters-buttons">
+                <p :class="{'ranking-filters-buttons-active': activeFilter === 1 }" @click="activeFilter = 1">Playerbase</p>
+                <p :class="{'ranking-filters-buttons-active': activeFilter === 2 }" @click="activeFilter = 2">Game</p>
+            </div>
         </div>
-        <weekly-chart :height="150"/>
+
+        <div v-if="activeFilter === 1">
+            <div class="content-introduction">
+                <p>Playerbase Statistics</p>
+            </div>
+            <weekly-chart :height="150"/>
+        </div>
+
+        <div v-if="activeFilter === 2">
+            <div class="content-introduction">
+                <p>Average player statistics around your level</p>
+            </div>
+            <user-stat-average-chart :height="150"/>
+        </div>
     </div>
 </template>
 <script>
 import weeklyUserChart from './weeklyUserChart'
+import userStatAverageChart from './userStatAverageChart'
 
 export default {
+    data() {
+        return {
+            activeFilter: 1
+        }
+    },
     components: {
-        'weekly-chart': weeklyUserChart
+        'weekly-chart': weeklyUserChart,
+        'user-stat-average-chart': userStatAverageChart
     }
 }
 </script>
---
+++ src/components/userStatAverageChart.vue
@@ -0,0 +1,40 @@
+<script>
+import { Bar } from 'vue-chartjs'
+
+Chart.defaults.global.defaultFontColor = '#9195d6'
+
+export default {
+  extends: Bar,
+  mounted () {
+    // Overwriting base render method with actual data.
+    this.renderChart({
+      labels: ['HP', 'Strength', 'Defense', 'Agility', 'Energy'],
+        defaultFontColor: 'white',
+      datasets: [
+        {
+          label: 'Average',
+          backgroundColor: [
+              'rgba(202, 69, 47, 0.2)',
+          ],
+          borderColor: [
+              'rgba(202,69,47,1)',
+          ],
+          borderWidth: 1,
+          data: [100, 32, 10, 5, 35]
+        },
+        {
+          label: 'You',
+          backgroundColor: [
+              'rgba(86, 79, 255, 0.2)',
+          ],
+          borderColor: [
+              'rgba(86,79,255,1)',
+          ],
+          borderWidth: 1,
+          data: [90, 15, 20, 9, 30]
+        }
+      ]
+    })
+  }
+}
+</script>
