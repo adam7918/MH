8a33869a611176e6ea23239cba4a58010de7e2f7
Adam Summerfield <summerfield.adam@gmail.com>
2018-07-30 00:43:04 +0200

    loadout page

--- src/components/InventoryPage__loadout.vue
+++ src/components/InventoryPage__loadout.vue
@@ -1,5 +1,152 @@
 <template>
-    <div class="placeholderdiv">
-        Loadout - Coming soon.
+    <div class="content">
+        <div class="ranking-filters-container">
+            <p style="color=#9093be"> HP: 100<br> Strength: 180<br> Defense: 0<br> Agility: 120<br> Energy: 0 </p>
+        </div>
+       
+        <div class="ranking-filters-container">
+            <p>Armor</p>
+        </div>
+        <div class="inventory-container" style="margin-top:-20px">
+        <div class="inventory-item item-purple">
+            <div class="inventory-item-header">
+                <div class="inventory-item-name">
+                    <p class="inventory-item-level">Lvl 30</p>
+                    <p>Weapon</p>
+                </div>
+                <p>Weapon</p>
+            </div>
+            <div class="inventory-item-stats">
+                <span>30 Strength</span>
+                <span>20 Agility</span>
+            </div>
+        </div>
+        <div class="inventory-item item-purple">
+            <div class="inventory-item-header">
+                <div class="inventory-item-name">
+                    <p class="inventory-item-level">Lvl 30</p>
+                    <p>Helmet</p>
+                </div>
+                <p>Helmet</p>
+            </div>
+            <div class="inventory-item-stats">
+                <span>30 Strength</span>
+                <span>20 Agility</span>
+            </div>
+        </div>
+                <div class="inventory-item item-purple">
+            <div class="inventory-item-header">
+                <div class="inventory-item-name">
+                    <p class="inventory-item-level">Lvl 30</p>
+                    <p>Torso</p>
+                </div>
+                <p>Torso</p>
+            </div>
+            <div class="inventory-item-stats">
+                <span>30 Strength</span>
+                <span>20 Agility</span>
+            </div>
+        </div>
+        <div class="inventory-item item-purple">
+            <div class="inventory-item-header">
+                <div class="inventory-item-name">
+                    <p class="inventory-item-level">Lvl 30</p>
+                    <p>Gauntlets</p>
+                </div>
+                <p>Gauntlets</p>
+            </div>
+            <div class="inventory-item-stats">
+                <span>30 Strength</span>
+                <span>20 Agility</span>
+            </div>
+        </div>
+        <div class="inventory-item item-purple">
+            <div class="inventory-item-header">
+                <div class="inventory-item-name">
+                    <p class="inventory-item-level">Lvl 30</p>
+                    <p>Greaves</p>
+                </div>
+                <p>Greaves</p>
+            </div>
+            <div class="inventory-item-stats">
+                <span>30 Strength</span>
+                <span>20 Agility</span>
+            </div>
+        </div>
+        <div class="inventory-item item-purple">
+            <div class="inventory-item-header">
+                <div class="inventory-item-name">
+                    <p class="inventory-item-level">Lvl 30</p>
+                    <p>Amulet</p>
+                </div>
+                <p>Amulet</p>
+            </div>
+            <div class="inventory-item-stats">
+                <span>30 Strength</span>
+                <span>20 Agility</span>
+            </div>
+        </div>
+        </div>
+        <div class="ranking-filters-container">
+            <p>Rune slot 1</p>
+        </div>
+        <div class="inventory-container" style="margin-top:-20px">
+        <div class="inventory-item item-blue">
+            <div class="inventory-item-header">
+                <div class="inventory-item-name">
+                    <p class="inventory-item-level">Lvl 30</p>
+                    <p>Rune of Placeholders</p>
+                </div>
+                <p>Rune</p>
+            </div>
+            <div class="inventory-item-stats">
+                <span>25% Chance to X on hit</span>
+            </div>
+        </div>
+        </div>
+        <div class="ranking-filters-container">
+            <p>Rune slot 2</p>
+        </div>
+        <div class="inventory-container" style="margin-top:-20px">
+        <div class="inventory-item item-purple">
+            <div class="inventory-item-header">
+                <div class="inventory-item-name">
+                    <p class="inventory-item-level">Lvl 30</p>
+                    <p>Rune of Placeholders</p>
+                </div>
+                <p>Rune</p>
+            </div>
+            <div class="inventory-item-stats">
+                <span>50% Chance to X on hit</span>
+            </div>
+        </div>
+        </div>
+        <div class="ranking-filters-container">
+            <p>Rune slot 3</p>
+        </div>
+        <div class="inventory-container" style="margin-top:-20px">
+        <div class="inventory-item">
+            <div class="inventory-item-header">
+                <div class="inventory-item-name">
+                    <p class="inventory-item-level"></p>
+                    <p></p>
+                </div>
+                <p></p>
+            </div>
+            <div class="inventory-item-stats">
+                <span>Rune slot unlocked at level 50</span>
+            </div>
+        </div>
+        </div>
     </div>
 </template>
+<script>
+
+export default {
+    data(){
+        return {
+
+        }
+    }
+}
+</script>
